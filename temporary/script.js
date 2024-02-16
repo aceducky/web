25d1a642ae05dc4eaf4db189261022ad3f4221cc
SrinivasD <115702816+aceducky@users.noreply.github.com>
2024-02-17 00:21:51 +0530

    modified:   temporary/index.html 	modified:   temporary/script.js

--- temporary/script.js
+++ temporary/script.js
@@ -1,64 +1,86 @@
 // removeBtn.innerHTML = "<i class='fas fa-trash'></i>";
+localStorage.clear();
 const taskListHeader = document.querySelector("#task-list-header");
-let completed = 0
-let inComplete = 0
+let completed = 0;
+let totalTasks = 0;
 function increaseCompleted() {
-    completed++;
-    document.querySelector('#completed') = completed
+  completed++;
+  document.querySelector("#completed").textContent = completed;
+}
+function decreaseCompleted() {
+  completed--;
+  document.querySelector("#completed").textContent = completed;
 }
 
-function increaseIncomplete() {
-    inComplete++;
-    document.querySelector('#inComplete') = inComplete
+function increaseTotalTasks() {
+  totalTasks++;
+  document.querySelector("#total-tasks").textContent = totalTasks;
+}
+function decreaseTotalTasks() {
+  totalTasks--;
+  document.querySelector("#total-tasks").textContent = totalTasks;
 }
 function present(el) {
-    document.body.appendChild(el)
+  document.body.appendChild(el);
 }
 
-let tasks = new Map()
-tasks.set('task 1', true)
-tasks.set('task 2', true)
-tasks.set('task 3', true)
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+console.log(tasks);
+
+tasks.push([ "task 1", true ]);
+console.log(tasks[0]); //output: {task 1: true}
+
+/*im getting this error:
+script.js:36 Uncaught TypeError: tasks[0].keys is not a function
+    at script.js:36:22
+    */
 
 // let task = Array.from(tasks.keys())[0];
-
-
 
 // tasks.set('task 2', true)
 // tasks.set('task 3', true)
 // tasks.set('task 4', true)
 function doWork(task) {
-    let task_el = document.createElement('div')
-    task_el.setAttribute('class', 'task-el')
-    let task_label = document.createElement('label')
-    task_label.setAttribute('class', 'task-label')
+  increaseTotalTasks();
+  let task_el = document.createElement("div");
+  task_el.setAttribute("class", "task-el");
+  let task_label = document.createElement("label");
+  task_label.setAttribute("class", "task-label");
 
-    let checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    task_label.appendChild(checkbox)
-    task_label.appendChild(document.createTextNode(task))
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  task_label.appendChild(checkbox);
+  task_label.appendChild(document.createTextNode(task));
 
-    let removeBtn = document.createElement('button')
-    removeBtn.innerHTML = 'x';
-    removeBtn.setAttribute('class', 'remove-btn')
+  let removeBtn = document.createElement("button");
+  removeBtn.innerHTML = "x";
+  removeBtn.setAttribute("class", "remove-btn");
 
-    task_el.appendChild(task_label)
-    task_el.appendChild(removeBtn)
-    taskListHeader.append(task_el)
+  task_el.appendChild(task_label);
+  task_el.appendChild(removeBtn);
+  taskListHeader.append(task_el);
 
-    removeBtn.addEventListener('click', (e) => {
-        e.target.parentElement.remove();
-    })
+  removeBtn.addEventListener("click", (e) => {
+    e.target.parentElement.remove();
+    if (completed > 0) {
+      decreaseCompleted();
+    }
+    decreaseTotalTasks();
+  });
 
-    checkbox.addEventListener('click', function () {
-        this.parentElement.style.textDecoration == 'line-through' ? (this.parentElement.style.textDecoration = 'none') : (this.parentElement.style.textDecoration = 'line-through');
-      });
-
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      this.parentElement.style.textDecoration = "line-through";
+      increaseCompleted();
+    } else {
+      this.parentElement.style.textDecoration = "";
+      if (completed > 0) {
+        decreaseCompleted();
+      }
+    }
+  });
 }
 
 for (const task of tasks) {
-    doWork(task[0])   
+  doWork(task[0]);
 }
-// for (let key of tasks.keys()) {
-//     doWork(key)
-// }
